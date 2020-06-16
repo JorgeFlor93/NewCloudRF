4901ef329903f910a026d5158a98f39adfc8a47a
JorgeFlor93 <fpjorge93@gmail.com>
2020-06-16 17:40:50 +0200

    area

--- Prop-tools/prop-tools.hh
+++ Prop-tools/prop-tools.hh
@@ -1,9 +1,15 @@
 #ifndef _PROPTOOLS_HH_
 #define _PROPTOOLS_HH_
 
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 #define DEG2RAD		1.74532925199e-02
 
 struct site {
@@ -14,5 +20,7 @@ struct site {
 
 double Distance(struct site site1, struct site site2);
 double miles_to_km(double miles);
+double ReadBearing(char *input);
+char *dec2dms(double decimal);
 
 #endif
--- newcloudrf.cc
+++ newcloudrf.cc
@@ -15,7 +15,16 @@ int main(int argc, char *argv[]){
     double Ploss = 0.0; //Ldb
     float f = 5000, d, height_Base = 100, height_Mobile = 2; //Tx_h y Rx_h, freq, distance
     int num_model = 0, mode = 2; //mode SUBURBAN
-    double wide_area = 0.0, height_area = 0.0, diagonal = 0.0;
+    double wide_area = 0.0, height_area = 0.0, degrees_numeric = 0.0;
+    char *tx_char_input;
+    // defrees minutes seconds
+    double tx_degrees = 55.895436;
+    std::cout.precision(6);
+    std::cout << "Grados decimales: " << std::fixed << tx_degrees << "\n";
+    tx_char_input = dec2dms(tx_degrees);
+    degrees_numeric = ReadBearing(tx_char_input);
+    std::cout << "char* d m s: " << tx_char_input << "\n";
+    std::cout << "vuelta a grados decimales: " << std::fixed << degrees_numeric << "\n";
 
     /*Tx*/
     struct site antennas[3];  //Constructor Vector de antenas
@@ -29,29 +38,25 @@ int main(int argc, char *argv[]){
     /*Rx*/
     struct site receptors[5];
 
-    receptors[0] = {55.835449, -3.380117, height_Mobile};
+    receptors[0] = {55.895017, -3.255537, height_Mobile};
 
-    receptors[1] = {55.821254, -3.298381, height_Mobile}; //bot right area
+    receptors[1] = {55.894908, -3.254606, height_Mobile}; //bot right area point
 
-    receptors[2] = {55.835449, -3.380117, height_Mobile}; // top left area
+    receptors[2] = {55.895017, -3.255537, height_Mobile}; // top left area point 
 
     /* AREA
-    *  min-right: 55.821254, -3.298381
-    *  max-left: 55.835449, -3.380117
+    *  min-right: 55.894908, -3.254606
+    *  max-left: 55.895017, -3.255537
     *  Area from antenna[0] to this min and max area.
-    */
+    *
 
-    // Wide and height area
+    * Wide and height area */
     receptors[3] = {receptors[1].lat, receptors[2].lon, height_Mobile}; // bot left point
-    receptors[4] = {receptors[2].lat, receptors[1].lon, height_Mobile}; //top right point
-    height_area = Distance(receptors[2], receptors[3]);
-    wide_area = Distance(receptors[2], receptors[4]);
-    diagonal = Distance(receptors[2], receptors[1]);
-    
-    std::cout << "Diagonal: " << diagonal << "\n";
-    std::cout << "Height: " << height_area << "\n";
-    std::cout << "Ancho: " << wide_area << "\n";
-    std::cout << "\n";
+    receptors[4] = {receptors[2].lat, receptors[1].lon, height_Mobile}; // top right point
+    height_area = Distance(receptors[2], receptors[3]); //km
+    wide_area = Distance(receptors[2], receptors[4]); //km
+
+    std::cout << "Height: " << height_area << ", Ancho: " << wide_area << "\n";
 
     std::cout << "1. fspl" << "\n";
     std::cout << "2. Hata" << "\n";
